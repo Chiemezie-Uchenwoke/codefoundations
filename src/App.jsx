782b171d37cb14aff5694c103b86cd6ee1632acc
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-04-04 15:56:15 +0100

    Add dependencies and create Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import {Routes, Route} from "react-router-dom"
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
-      <h1>Hello World!</h1>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
     </>
   )
 }
