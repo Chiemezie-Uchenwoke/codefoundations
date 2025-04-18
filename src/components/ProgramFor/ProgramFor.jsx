41b4e9f4d2abdd317f51efee66b2b2de684c3e75
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-04-18 23:18:48 +0100

    complete section

--- src/components/ProgramFor/ProgramFor.jsx
+++ src/components/ProgramFor/ProgramFor.jsx
@@ -3,32 +3,32 @@ import { FaCheckCircle } from "react-icons/fa";
 
 const ProgramFor = () => {
     return (
-        <section className="container-custom top-margin flex flex-col items-center gap-6">
-            <h2 className="font-bold capitalize text-xl">Who is this program for?</h2>
-            <div className="">
-                <figure>
-                    <img src={youngDevs} alt="Image of young people working together" loading="lazy" />
+        <section className="container-custom top-margin flex flex-col items-center gap-6 md:gap-8 lg:gap-10">
+            <h2 className="font-semibold capitalize text-xl sm:text-2xl lg:text-4xl">Who is this program for?</h2>
+            <div className="flex flex-col md:flex-row gap-4 sm:gap-8">
+                <figure className="overflow-hidden w-full md:w-[50%] lg:w-[40%]">
+                    <img src={youngDevs} alt="Young developers learning and collaborating" loading="lazy" className="w-full h-full object-fit-cover" />
                 </figure>
 
-                <ul>
-                    <h3>This program is perfect for you if:</h3>
-                    <li>
-                        <FaCheckCircle /> 
+                <ul className="w-full md:w-[50%] lg:w-[60%] flex flex-col gap-4">
+                    <h3 className="font-semibold text-base lg:text-lg">This program is perfect for you if:</h3>
+                    <li className="text-sm lg:text-base flex gap-2 leading-6">
+                        <span className="py-2"><FaCheckCircle className="text-base text-prim" /></span> 
                         You're a beginner who wants to learn web development the right way, not just from random YouTube tutorials.
                     </li>
 
-                    <li>
-                        <FaCheckCircle /> 
+                    <li className="text-sm lg:text-base flex gap-2 leading-6">
+                        <span className="py-2"><FaCheckCircle className="text-base text-prim" /></span>  
                         You're self-taught but feel stuck, especially with JavaScript.
                     </li>
 
-                    <li>
-                        <FaCheckCircle /> 
+                    <li className="text-sm lg:text-base flex gap-2 leading-6">
+                        <span className="py-2"><FaCheckCircle className="text-base text-prim" /></span> 
                         You've built some projects, but don't fully understand how things work under the hood.
                     </li>
 
-                    <li>
-                        <FaCheckCircle /> 
+                    <li className="text-sm lg:text-base flex gap-2 leading-6">
+                        <span className="py-2"><FaCheckCircle className="text-base text-prim" /></span> 
                         You feel overwhelmed trying to learn everything alone — you need structure, support, and real feedback.
                     </li>
                 </ul>
