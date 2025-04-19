17c745da7ead3e2157bbea12dae2aaba6eb701cf
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-04-19 02:33:46 +0100

    complete program features

--- src/components/ProgramFeatures/ProgramFeatures.jsx
+++ src/components/ProgramFeatures/ProgramFeatures.jsx
@@ -43,7 +43,7 @@ const ProgramFeatures = () => {
                                 Perfect for fast learners who want to get started quickly
                             </li>
                         </ul>
-                        <Link to="#" className="underline mt-3 text-prim text-sm font-semibold capitalize">view curriculum</Link>
+                        <Link to="#" className="underline mt-3 text-prim text-sm font-semibold capitalize hover:no-underline">view curriculum</Link>
                     </div>
 
                     <div className="group border border-borderColor py-8 px-4 flex flex-col gap-3 shadow rounded-lg bg-white-lite hover:shadow-xl hover:bg-white-dark hover:scale-[1.005] duration-300">
@@ -78,7 +78,7 @@ const ProgramFeatures = () => {
                                 Ideal for beginners who want a strong foundation in web development
                             </li>
                         </ul>
-                        <Link to="#" className="underline mt-3 text-prim text-sm font-semibold capitalize">view curriculum</Link>
+                        <Link to="#" className="underline mt-3 text-prim text-sm font-semibold capitalize hover:no-underline">view curriculum</Link>
                     </div>
                     
                     <div className="border border-borderColor group py-8 px-4 flex flex-col gap-3 shadow rounded-lg bg-white-lite hover:shadow-xl hover:bg-white-dark hover:scale-[1.005] duration-300">
@@ -113,7 +113,7 @@ const ProgramFeatures = () => {
                                 Perfect for those who already know HTML & CSS
                             </li>
                         </ul>
-                        <Link to="#" className="underline mt-3 text-prim text-sm font-semibold capitalize">view curriculum</Link>
+                        <Link to="#" className="underline mt-3 text-prim text-sm font-semibold capitalize hover:no-underline">view curriculum</Link>
                     </div>
 
                     <div className="group border border-borderColor py-8 px-4 flex flex-col gap-3 shadow rounded-lg bg-white-lite hover:shadow-xl hover:bg-white-dark hover:scale-[1.005] duration-300">
@@ -148,7 +148,7 @@ const ProgramFeatures = () => {
                                 Best for devs ready to level up their JavaScript skills
                             </li>
                         </ul>
-                        <Link to="#" className="underline mt-3 text-prim text-sm font-semibold capitalize">view curriculum</Link>
+                        <Link to="#" className="underline mt-3 text-prim text-sm font-semibold capitalize hover:no-underline">view curriculum</Link>
                     </div>
                 </div>
             </div>
