c303671ce01586ba5916e8cfe425cb082dae2e59
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-04-20 18:29:19 +0100

    Add pricing component

--- src/components/Pricing/Pricing.jsx
+++ src/components/Pricing/Pricing.jsx
@@ -1,7 +1,9 @@
+import SectionHeadingText from "../SectionHeadingText/SectionHeadingText";
+
 const Pricing = () => {
     return (
-        <section className="top-margin">
-
+        <section className="top-margin container-custom flex flex-col gap-6 md:gap-8 lg:gap-10 items-center">
+            <SectionHeadingText headingText="Pricing" />
         </section>
     )
 }
