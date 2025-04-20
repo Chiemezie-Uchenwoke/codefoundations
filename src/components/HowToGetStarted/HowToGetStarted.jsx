2496f2f255b74cc7ab6a39464fbbb59cc1b2993b
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-04-20 17:55:14 +0100

    complete steps section

--- src/components/HowToGetStarted/HowToGetStarted.jsx
+++ src/components/HowToGetStarted/HowToGetStarted.jsx
@@ -2,18 +2,18 @@ import SectionHeadingText from "../SectionHeadingText/SectionHeadingText";
 
 const HowToGetStarted = () => {
     return (
-        <section className="top-margin mb-12">
+        <section className="top-margin">
             <div className="container-custom flex flex-col gap-6 md:gap-8 lg:gap-10 items-center">
                 <SectionHeadingText headingText="How to Join the Mentorship" />
 
                 <div className="grid grid-cols-1 gap-8 w-full sm:grid-cols-2 lg:grid-cols-4">
-                    <div className="bg-prim text-white-lite p-4 rounded-md flex items-center justify-center text-center text-base border-2 border-borderColor shadow-md capitalize">Fill the interest form</div>
+                    <div className="bg-prim/5 text-shade-of-black p-4 rounded-md flex items-center justify-center text-center text-sm lg:text-base border border-borderColor shadow-md capitalize">Fill the interest form</div>
 
-                    <div className="bg-prim text-white-lite p-4 rounded-md flex items-center justify-center text-center text-base border-2 border-borderColor shadow-md capitalize">Choose your track - Web or JavaScript</div>
+                    <div className="bg-prim/5 text-shade-of-black p-4 rounded-md flex items-center justify-center text-center text-sm lg:text-base border border-borderColor shadow-md capitalize">Choose your track - Web or JavaScript</div>
 
-                    <div className="bg-prim text-white-lite p-4 rounded-md flex items-center justify-center text-center text-base border-2 border-borderColor shadow-md capitalize">Make payment on Selar</div>
+                    <div className="bg-prim/5 text-shade-of-black p-4 rounded-md flex items-center justify-center text-center text-sm lg:text-base border border-borderColor shadow-md capitalize">Make payment on Selar</div>
 
-                    <div className="bg-prim text-white-lite p-4 rounded-md flex items-center justify-center text-center text-base border-2 border-borderColor shadow-md capitalize">Receive onboarding email + get started</div>
+                    <div className="bg-prim/5 text-shade-of-black p-4 rounded-md flex items-center justify-center text-center text-sm lg:text-base border border-borderColor shadow-md capitalize">Receive onboarding email + get started</div>
                 </div>
             </div>
         </section>
