160c9a0debae2d2e22e8d2965f4053d3f688059b
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-05-05 17:56:23 +0100

    Add footer to curriculum pages

--- src/pages/AdvancedWebFundamentals.jsx
+++ src/pages/AdvancedWebFundamentals.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header/Header";
 import AdvancedWebCourse from "../components/AdvancedWebCourse/AdvancedWebCourse";
+import Footer from "../components/Footer/Footer"
 
 const AdvancedWebFundamentals = () => {
     return (
         <>
             <Header />
             <AdvancedWebCourse />
+            <Footer />
         </>
     )
 }
--- src/pages/WebFundamentals.jsx
+++ src/pages/WebFundamentals.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header/Header";
 import BeginnerCourse from "../components/BeginnerCourse/BeginnerCourse";
+import Footer from "../components/Footer/Footer"
 
 const WebFundamentals = () => {
     return (
         <>
             <Header />
             <BeginnerCourse />
+            <Footer />
         </>
     )
 }
